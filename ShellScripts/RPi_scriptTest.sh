4c8826b9707d7c9c4337fb31b2dcfbf2313a28d3
Gundeep Saini <36812339+gundeepsaini@users.noreply.github.com>
2020-03-22 18:35:05 +0100

    Create RPi_scriptTest.sh

---
+++ ShellScripts/RPi_scriptTest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# https://www.circuitbasics.com/how-to-write-and-run-a-shell-script-on-the-raspberry-pi/
+# https://core-electronics.com.au/tutorials/basics-writing-your-first-script-with-raspberry-pi.html
+
+# sudo chmod +x hello-world.sh  <---- to make executable
+# sh hello-world.sh   			<---- to run
+
+echo "Hello World!"
