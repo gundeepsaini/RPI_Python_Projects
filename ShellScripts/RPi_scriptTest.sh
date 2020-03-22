2f3ef4bad572d1b324080ded68c1eb1e8f5a4436
Gundeep Saini <36812339+gundeepsaini@users.noreply.github.com>
2020-03-22 18:58:15 +0100

    added gitsync

--- ShellScripts/RPi_scriptTest.sh
+++ ShellScripts/RPi_scriptTest.sh
@@ -3,8 +3,8 @@
 # https://www.circuitbasics.com/how-to-write-and-run-a-shell-script-on-the-raspberry-pi/
 # https://core-electronics.com.au/tutorials/basics-writing-your-first-script-with-raspberry-pi.html
 
-# sudo chmod +x hello-world.sh  <---- to make executable
-# sh hello-world.sh   			<---- to run
+# sudo chmod +x RPi_scriptTest.sh  <---- to make executable
+# sh RPi_scriptTest.sh   	   <---- to run
 
 echo "Hello World!"
 
---
+++ ShellScripts/RPi_syncGit_Scripts_v01.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# RPi_syncGit_Scripts_v01.sh
+
+# Make Executable: 		sudo chmod +x RPi_syncGit_Scripts_v01.sh  
+# Run:					sh RPi_syncGit_Scripts_v01.sh
+
+echo "Sync Scripts"
+echo "This will discard all local changes!!!!"
+read -p "Press enter to continue"
+	
+cd /home/pi/Desktop/GitHub_Rpi/RPI_Python_Projects
+git checkout master
+git reset --hard
+git pull https://github.com/gundeepsaini/RPI_Python_Projects.git
+# ls
+
+read -n 1 -s -r -p "Press any key to continue"
